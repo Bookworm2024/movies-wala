114d63dca1692ccefd4792f36526f97685a958d1
BookWorm <infohubnetworks@gmail.com>
2024-11-16 20:57:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -126,7 +126,7 @@ SHORTLINK_API = environ.get('SHORTLINK_API', '28cb820c966371de4aff06fc22d6a8a0bc
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 PORT = environ.get("PORT", "8080")
-MSG_ALRT = environ.get('MSG_ALRT', 'Aye, Aye Caption Jack Sparrow! ✨')
+MSG_ALRT = environ.get('MSG_ALRT', 'Aye, Aye Captain Jack Sparrow! ✨')
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")
